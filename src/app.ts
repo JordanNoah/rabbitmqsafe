003fa5db6fa1a3d0129ba40b04292d8444fe5ede
jordan.ubilla <jordan.ubilla@funiber.org>
2024-01-23 15:25:29 -0500

    rabbit safe

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import { AppRoutes } from "./presentation/routes"
+import { Server } from "./presentation/server"
+
+(()=>{
+    main()
+})()
+
+async function main() {
+    new Server({routes:AppRoutes.routes}).start()
+}
